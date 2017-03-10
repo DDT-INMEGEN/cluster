e640704493df0ac4a518bb4683e5b31852ccaa25
Joshua Haase <hahj87@gmail.com>
2017-03-09 20:17:26 -0600

    Ejecutar configurador después de sincronizar.
    - Subir cambios después de hacerlos.

--- cluster/scripts/probar-rama.sh
+++ cluster/scripts/probar-rama.sh
@@ -2,3 +2,7 @@
 RAMA="$1"
 sudo etckeeper commit -m "Antes de probar rama $RAMA"
 sudo etckeeper vcs pull origin $RAMA -X theirs -s recursive --no-edit
+sudo etckeeper init
+sudo /etc/cron.hourly/configurador
+sudo etckeeper commit -m "Después de probar rama $RAMA"
+sudo etckeeper vcs push origin
