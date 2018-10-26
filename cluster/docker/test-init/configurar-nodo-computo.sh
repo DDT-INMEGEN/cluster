6a3a8fa786394d9997a2d2af54d58e8713958a6e
Joshua Haase <hahj87@gmail.com>
2018-10-25 19:17:04 -0500

    configurar-nodo-computo.sh can build a docker image.
    ```
    /bin/cd cluster mk test
    ```
    
    builds an image that contains a prototype node for testing.
    
    The script `test-init` can run the container for troubleshooting.
    
    The process for transforming a vanilla docker container into a node
    was documented into `configurar-nodo-computo.sh`.
    
    That container was transformed into an image using:
    
    ```
    CONTAINER_ID=
    docker commit $CONTAINER_ID cluster-node:just-added
    ```

--- cluster/docker/test-init/configurar-nodo-computo.sh
+++ cluster/docker/test-init/configurar-nodo-computo.sh
@@ -8,6 +8,7 @@ apt-get install -y 9base execline etckeeper
 #Copiar la llave privada de computo para solo R de fai
 install ~root/.ssh -m 700 -d
 curl -L repo.cluster.inmegen.gob.mx/computo.key | tee ~root/.ssh/id_rsa
+curl -L repo.cluster.inmegen.gob.mx/known_hosts | tee ~root/.ssh/known_hosts
 chmod go-r ~root/.ssh/id_rsa
 
 #Sincronizamos etckeeper
@@ -18,6 +19,3 @@ etckeeper vcs remote remove origin
 etckeeper vcs remote add origin gitolite3@git.cluster.inmegen.gob.mx:etckeeper.git
 etckeeper vcs pull origin auto-conf -X theirs -s recursive --no-edit
 etckeeper vcs branch -m `hostname`
-
-# configurar port-mirror
-/usr/lib/plan9/bin/mk -f /etc/cluster/mkfile port_mirror
