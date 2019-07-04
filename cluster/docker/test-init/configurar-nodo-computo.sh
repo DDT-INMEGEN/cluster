a30b014dae953ab5c7dd085dec3602683167e144
Joshua Haase <hahj87@gmail.com>
2019-07-04 16:42:04 -0500

    #82 Configurar clave pública antes de instalar.

--- cluster/docker/test-init/configurar-nodo-computo.sh
+++ cluster/docker/test-init/configurar-nodo-computo.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -x
 
-#Prerequisitos
-apt update
-apt-get install -y 9base execline etckeeper
-
-#Copiar la llave privada de computo para solo R de fai
+# Copiar la llave privada de computo para solo R de fai
 install ~root/.ssh -m 700 -d
 curl -L repo.cluster.inmegen.gob.mx/computo.key | tee ~root/.ssh/id_rsa
 curl -L repo.cluster.inmegen.gob.mx/known_hosts | tee ~root/.ssh/known_hosts
 chmod go-r ~root/.ssh/id_rsa
+
+# Requisitos de instalación
+curl repo.cluster.inmegen.gob.mx/supercomputo.asc | apt-key add -
+apt update
+apt-get install -y 9base execline etckeeper
 
 #Sincronizamos etckeeper
 git config --global user.email `hostname`"@cluster.inmegen.gob.mx"
@@ -17,5 +18,5 @@ git config --global user.name `hostname`
 etckeeper commit -m 'Original config.'
 etckeeper vcs remote remove origin
 etckeeper vcs remote add origin gitolite3@git.cluster.inmegen.gob.mx:etckeeper.git
-etckeeper vcs pull origin auto-conf -X theirs -s recursive --no-edit
+etckeeper vcs pull origin auto-conf -X theirs -s recursive --no-edit --allow-unrelated
 etckeeper vcs branch -m `hostname`
